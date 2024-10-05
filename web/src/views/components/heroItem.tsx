51c5c94241ea50eefc6c052600a82bcf0766886d
Renara Secchim <renarasecchim@gmail.com>
2024-10-05 10:29:32 -0300

    feat: :sparkles: add removeHeroe and updateHeroe functions to HeroItem component

--- web/src/views/components/heroItem.tsx
+++ web/src/views/components/heroItem.tsx
@@ -1,18 +1,28 @@
 import { IHero } from "@app/entities/IHero";
+import globalStore from "@app/store/globalStore";
 import { MapPinCheckInside, PenLine, Swords, Trash2 } from "lucide-react";
 
 import Button from "./button";
 import Label from "./label";
 
 function HeroItem({ ...props }: IHero) {
+  const removeHeroe = globalStore.useStore((state) => state.removeHero);
+  const updateHeroe = globalStore.useStore((state) => state.updateHeroe);
+
   return (
     <div className="flex flex-col w-60 p-2 rounded-xl border drop-shadow-sm gap-4">
       <div className="flex justify-between">
-        <Button className="bg-transparent w-11">
+        <Button
+          className="bg-transparent w-11"
+          onClick={() => updateHeroe(props.id)}
+        >
           <PenLine color="#000000" />
         </Button>
 
-        <Button className="bg-transparent w-11">
+        <Button
+          className="bg-transparent w-11"
+          onClick={() => removeHeroe(props.id)}
+        >
           <Trash2 color="#e65555" />
         </Button>
       </div>
