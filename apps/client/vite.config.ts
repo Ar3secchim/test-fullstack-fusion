23c6f3b1fc71cd0f2f0c13df936be43b753fb16b
Renara Secchim <renarasecchim@gmail.com>
2024-10-24 18:59:51 -0300

    Refactor .gitignore and turbo.json files

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -7,7 +7,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
     proxy: {
       "/api": {
         target: "http://localhost:3000",
