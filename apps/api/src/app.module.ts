79503e7f571672432a983949696d3f43dcdaddeb
Renara Secchim <renarasecchim@gmail.com>
2024-10-25 10:13:46 -0300

    refactor: :recycle: static file serving configuration in app.module.ts, vite.config.ts, and vercel.json

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -14,6 +14,7 @@ import { ValidateHeroModule } from './validHeroes/validHeroes.module';
 
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../', 'client/dist'),
+      exclude: ['api/*'],
     }),
 
     HeroModule,
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     proxy: {
       "/api": {
         target: "http://localhost:3000",
